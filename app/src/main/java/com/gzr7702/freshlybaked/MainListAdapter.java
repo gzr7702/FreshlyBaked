27316495052ac9f13044011c593c9e52161a15d1
gzr7702 <rgraziano.gzr7702@gmail.com>
2017-10-14 09:00:53 -0400

    feat: added cards for main view

--- app/src/main/java/com/gzr7702/freshlybaked/MainListAdapter.java
+++ app/src/main/java/com/gzr7702/freshlybaked/MainListAdapter.java
@@ -1,9 +1,14 @@
 package com.gzr7702.freshlybaked;
 
+import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.gzr7702.freshlybaked.dummy.DummyContent;
 
 /**
  * Created by rob on 10/12/17.
@@ -12,45 +17,38 @@ import android.widget.TextView;
 public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ViewHolder> {
     private String[] mDataset;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
-            super(v);
-            mTextView = v;
+        public TextView recipeText;
+        public ImageView recipeImage;
+
+        public ViewHolder(CardView view) {
+            super(view);
+            recipeText = (TextView) view.findViewById(R.id.recipe_row_text);
+            recipeImage = (ImageView) view.findViewById(R.id.recipe_row_iamge);
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public MainListAdapter(String[] myDataset) {
         mDataset = myDataset;
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public MainListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
-        // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.recipe_row, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        ViewHolder vh = new ViewHolder(v);
+
+        ViewHolder vh = new ViewHolder(cv);
         return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(MainListAdapter.ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset[position]);
-
+        holder.recipeText.setText(mDataset[position]);
+        holder.recipeImage.setImageResource(R.drawable.cake);
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mDataset.length;
