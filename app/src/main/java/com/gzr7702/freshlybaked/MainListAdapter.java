7d067093f6bdb9307d757f8bce9c2fb0579675f0
gzr7702 <rgraziano.gzr7702@gmail.com>
2017-10-14 09:12:22 -0400

    feat: functionality to launch recipe activity

--- app/src/main/java/com/gzr7702/freshlybaked/MainListAdapter.java
+++ app/src/main/java/com/gzr7702/freshlybaked/MainListAdapter.java
@@ -1,9 +1,11 @@
 package com.gzr7702.freshlybaked;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,11 +21,13 @@ public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ViewHo
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
+        public View recipeView;
         public TextView recipeText;
         public ImageView recipeImage;
 
         public ViewHolder(CardView view) {
             super(view);
+            recipeView = view;
             recipeText = (TextView) view.findViewById(R.id.recipe_row_text);
             recipeImage = (ImageView) view.findViewById(R.id.recipe_row_iamge);
         }
@@ -47,6 +51,16 @@ public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ViewHo
     public void onBindViewHolder(MainListAdapter.ViewHolder holder, int position) {
         holder.recipeText.setText(mDataset[position]);
         holder.recipeImage.setImageResource(R.drawable.cake);
+
+        holder.recipeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, RecipeActivity.class);
+
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
